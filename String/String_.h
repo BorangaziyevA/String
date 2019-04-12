7688adbefe2694108adfc9eaaf7b6a4f74920eb0
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-12 20:23:47 +0600

    Update String_.h

--- String/String_.h
+++ String/String_.h
@@ -12,7 +12,7 @@ class String_
 public:
 	String_();
 	String_(const char *s);
-	String_(const String_ &s);
+	String_(const String_ &obj);
 	~String_();
 
 	void setString(const char *s);
@@ -22,5 +22,8 @@ public:
 	void append(const char *s);
 	void print();
 	void clear();
+	String_ operator+=(const char*s);
+	String_ operator+=(String_ s);
+	String_& operator=(const String_ &obj);
 };
 
