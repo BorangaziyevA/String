a2527cd955c40d981de6a87bb30d096dffbb61c2
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-23 21:57:11 +0600

    Update String_.h

--- String/String_.h
+++ String/String_.h
@@ -13,8 +13,10 @@ public:
 	String_();
 	String_(const char *s);
 	String_(const String_ &obj);
+	String_(String_ && right);
 	~String_();
 
+	String_& operator=(String_ && right);
 	void setString(const char *s);
 	const char* getString();
 	bool equal(const char *s);
@@ -25,5 +27,11 @@ public:
 	String_ operator+=(const char*s);
 	String_ operator+=(String_ s);
 	String_& operator=(const String_ &obj);
+	friend bool operator==(const string& a, const string& b);
+	friend bool operator!=(const string& a, const string& b);
 };
 
+ostream& operator<<(ostream& os, const String_& obj);
+istream& operator>>(istream& is, String_& obj);
+bool operator==(const string& a, const string& b);
+bool operator!=(const string& a, const string& b);
